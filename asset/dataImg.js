0856af2ae57d6adda234295e6fefb440b58524d2
Dev <dev@gmail..com>
2026-05-22 13:03:42 +0700

    Fourt commit

---
+++ asset/dataImg.js
@@ -0,0 +1,16 @@
+const dataSet = {
+    Images01: {
+        label: "Rainwear Series",
+        cat: "https://res.cloudinary.com/dag73dhpl/image/upload/v1779423494/6eeae770-2cdb-4102-9ec7-378c271d656e_fj5thx.jpg",
+        clothing: "https://res.cloudinary.com/dag73dhpl/image/upload/v1779422816/s_k1fg0t.jpg",
+        cat_clothing: "https://res.cloudinary.com/dag73dhpl/image/upload/v1779423321/f98be5a3-c4a8-48e8-9cb4-9487142ba982_1_kpomeh.jpg",
+        cat_clothing_package: "https://res.cloudinary.com/dag73dhpl/image/upload/v1779427571/upscalemedia-transformed_2_tbzq5j.jpg"
+    },
+    Images02: {
+        label: "Series 02",
+        cat: "",
+        clothing: "",
+        cat_clothing: "",
+        cat_clothing_package: ""
+    }
+};
