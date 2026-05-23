46fec1866b797c568e19e5c2ce1a4d11c1a854d4
Dev <dev@gmail..com>
2026-05-23 14:04:47 +0700

    Update image new 3

--- asset/dataImg.js
+++ asset/dataImg.js
@@ -19,5 +19,12 @@ const dataSet = {
         clothing:"https://res.cloudinary.com/dag73dhpl/image/upload/v1779500679/Gemini_Generated_Image_aosvfaosvfaosvfa_a13emn_1_ba5twt.jpg",
         cat_clothing: "https://res.cloudinary.com/dag73dhpl/image/upload/v1779500569/a70a6db1-41a6-4925-83b4-180fe9f6fa44_1_ncrhp7.jpg",
         cat_clothing_package: "https://res.cloudinary.com/dag73dhpl/image/upload/v1779500570/6a581454-c56f-46f9-94b4-aa364e92da29_1_ixx1tx.jpg"
+    },
+    Images04: {
+        label: "[ABCat] Thai Songkran Heritage Set",
+        cat: "https://res.cloudinary.com/dag73dhpl/image/upload/v1779518647/Gemini_Generated_Image_6ultua6ultua6ult_tkjl3h_bixaww.jpg",
+        clothing: "https://res.cloudinary.com/dag73dhpl/image/upload/v1779518437/a5fb3952-e668-43f2-a54d-5684a58163db_1_fkk7wb.jpg",
+        cat_clothing: "https://res.cloudinary.com/dag73dhpl/image/upload/v1779518646/Gemini_Generated_Image_g0ga9bg0ga9bg0ga_zqrsxz_xrpn6k.jpg",
+        cat_clothing_package: "https://res.cloudinary.com/dag73dhpl/image/upload/v1779518648/7f13a047-f417-49a7-ae45-8e5030968a91_uxfc5l.jpg",
     }
 };
