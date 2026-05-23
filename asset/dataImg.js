7f93e13bfe60eaca8862152ac133059efbb6e130
Dev <dev@gmail..com>
2026-05-23 08:49:15 +0700

    Update image new

--- asset/dataImg.js
+++ asset/dataImg.js
@@ -1,23 +1,23 @@
 const dataSet = {
     Images01: {
-        label: "[ABCat] Rainwear Series",
+        label: "[ABCat] Yellow Paws Raincoat & Booties Set",
         cat: "https://res.cloudinary.com/dag73dhpl/image/upload/v1779423494/6eeae770-2cdb-4102-9ec7-378c271d656e_fj5thx.jpg",
         clothing: "https://res.cloudinary.com/dag73dhpl/image/upload/v1779422816/s_k1fg0t.jpg",
         cat_clothing: "https://res.cloudinary.com/dag73dhpl/image/upload/v1779423321/f98be5a3-c4a8-48e8-9cb4-9487142ba982_1_kpomeh.jpg",
         cat_clothing_package: "https://res.cloudinary.com/dag73dhpl/image/upload/v1779427571/upscalemedia-transformed_2_tbzq5j.jpg"
     },
     Images02: {
-        label: "[ABCat] Blue Aloha",
+        label: "[ABCat] Blue Hawaiian Aloha Shirt & Straw Hat Set",
         cat: "https://res.cloudinary.com/dag73dhpl/image/upload/v1779432500/upscalemedia-transformed_2_tpsyaj.jpg",
         clothing: "https://res.cloudinary.com/dag73dhpl/image/upload/v1779432500/upscalemedia-transformed_3_hwen66.jpg",
         cat_clothing: "https://res.cloudinary.com/dag73dhpl/image/upload/v1779432499/upscalemedia-transformed_4_veksp3.jpg",
-        cat_clothing_package: "https://res.cloudinary.com/dag73dhpl/image/upload/v1779431914/upscalemedia-transformed_2_ibqs0y.jpg"
+        cat_clothing_package: "https://res.cloudinary.com/dag73dhpl/image/upload/v1779495968/6eeae770-2cdb-4102-9ec7-378c271d656e_1_rgc7go.jpg"
     },
     Images03: {
-        label: "[ABCat] ",
-        cat: "",
-        clothing:"",
-        cat_clothing: "",
-        cat_clothing_package: ""
+        label: "[ABCat] Cozy Knit Winter Set",
+        cat: "https://res.cloudinary.com/dag73dhpl/image/upload/v1779500568/Gemini_Generated_Image_2jtnof2jtnof2jtn_s6qda4_1_hfii3z.jpg",
+        clothing:"https://res.cloudinary.com/dag73dhpl/image/upload/v1779500679/Gemini_Generated_Image_aosvfaosvfaosvfa_a13emn_1_ba5twt.jpg",
+        cat_clothing: "https://res.cloudinary.com/dag73dhpl/image/upload/v1779500569/a70a6db1-41a6-4925-83b4-180fe9f6fa44_1_ncrhp7.jpg",
+        cat_clothing_package: "https://res.cloudinary.com/dag73dhpl/image/upload/v1779500570/6a581454-c56f-46f9-94b4-aa364e92da29_1_ixx1tx.jpg"
     }
 };
